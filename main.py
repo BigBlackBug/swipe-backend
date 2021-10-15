794d2348d95e9a6a64d236c804c0a0c7cf7b4595
Evgeny Shakhmae <bigblackbug@harddays.dev>
2021-10-15 14:29:09 +0300

    add alembic migration step to main.py

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import logging
 import sys
+from pathlib import Path
 
+import alembic.command
+import alembic.config
 import uvicorn
 from fastapi import FastAPI
 
@@ -16,13 +19,22 @@ logging.basicConfig(stream=sys.stderr,
                     level=logging.DEBUG)
 
 logger = logging.getLogger(__name__)
-app = FastAPI(docs_url=f'/docs', redoc_url=f'/redoc')
 
+app = FastAPI(docs_url=f'/docs', redoc_url=f'/redoc')
 app.include_router(users.endpoints.me_router)
 app.include_router(users.endpoints.users_router)
 app.include_router(swipe.endpoints.router)
 
 if __name__ == '__main__':
+    migrations_dir = str(Path('migrations').absolute())
+    logger.info(
+        f'Running DB migrations in {migrations_dir} '
+        f'on {settings.DATABASE_URL}')
+    alembic_cfg = alembic.config.Config('alembic.ini')
+    alembic_cfg.set_main_option('script_location', migrations_dir)
+    alembic_cfg.set_main_option('sqlalchemy.url', settings.DATABASE_URL)
+    alembic.command.upgrade(alembic_cfg, 'head')
+
     CloudStorage().initialize_storage()
     logger.info(f'Starting app at port {settings.PORT}')
     uvicorn.run('main:app', host='0.0.0.0',
--- start.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-echo 'Running migrations'
-alembic upgrade head
-
-echo 'Starting swipe backend'
-python main.py
