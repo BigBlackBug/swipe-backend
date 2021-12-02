bdb51a8aba8ad0d2ee9191c2669cf1c371469119
Evgeny Shakhmae <bigblackbug@harddays.dev>
2021-12-02 15:42:45 +0300

    remove users with chats from the matchmaking connection list

--- swipe/matchmaking/services.py
+++ swipe/matchmaking/services.py
@@ -4,10 +4,11 @@ from typing import Optional, Iterable
 
 from dateutil.relativedelta import relativedelta
 from fastapi import Depends
-from sqlalchemy import select, cast, String
+from sqlalchemy import select, cast, String, union_all
 from sqlalchemy.orm import Session, load_only
 
 import swipe.swipe_server.misc.dependencies as dependencies
+from swipe.swipe_server.chats.models import Chat
 from swipe.swipe_server.users.enums import Gender
 from swipe.swipe_server.users.models import User
 
@@ -31,12 +32,22 @@ class MMUserService:
         max_age = datetime.datetime.utcnow() + relativedelta(
             years=age + age_difference)
 
+        a_to_b = select(Chat.the_other_person_id).where(
+            Chat.initiator_id == current_user_id
+        )
+        b_to_a = select(Chat.initiator_id).where(
+            Chat.the_other_person_id == current_user_id
+        )
+        users_with_chat = self.db.execute(
+            union_all(a_to_b, b_to_a)).scalars().all()
         query = select(cast(User.id, String)). \
             where(gender_clause). \
             where(User.date_of_birth.between(min_age, max_age)). \
             where(User.id != current_user_id). \
+            where(User.id.not_in(users_with_chat)). \
             where(User.id.in_(online_users)). \
             where(~User.blocked_by.any(id=current_user_id))
+
         return self.db.execute(query).scalars().all()
 
     def get_matchmaking_preview(self, user_id: str):
