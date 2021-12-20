ea7794e2defffce5ebc5c4f8200c95eeb32401fd
Evgeny Shakhmae <bigblackbug@harddays.dev>
2021-12-21 00:45:20 +0300

    fix setting message date

--- swipe/chat_server/services.py
+++ swipe/chat_server/services.py
@@ -41,18 +41,14 @@ class ChatServerRequestProcessor:
                 recipient_id=data.recipient_id,
                 message=payload.text,
                 image_id=payload.image_id,
-                # it's a stub, because some devices might have
-                # their local time fucked up
-                timestamp=datetime.datetime.utcnow()
+                timestamp=payload.timestamp
             )
         elif isinstance(payload, GlobalMessagePayload):
             self.chat_service.post_message_to_global(
                 message_id=payload.message_id,
                 sender_id=data.sender_id,
                 message=payload.text,
-                # it's a stub, because some devices might have
-                # their local time fucked up
-                timestamp=datetime.datetime.utcnow()
+                timestamp=payload.timestamp
             )
         elif isinstance(payload, MessageStatusPayload):
             message_status = MessageStatus.__members__[payload.status.upper()]
